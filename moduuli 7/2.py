da428fd43b12ab37410f9c2179ac46562011c8a6
unclejoona <joona.rajaniemi1999@gmail.com>
2024-09-18 11:53:09 +0300

    moduuli 6

--- moduuli 7/2.py
+++ moduuli 7/2.py
@@ -2,20 +2,18 @@
 # Kunkin nimen syöttämisen jälkeen ohjelma tulostaa joko tekstin Uusi nimi tai Aiemmin syötetty nimi sen mukaan,
 # syötettiinkö nimi ensimmäistä kertaa. Lopuksi ohjelma luettelee syötetyt nimet yksi kerrallaan allekkain mielivaltaisessa järjestyksessä.
 # Käytä joukkotietorakennetta nimien tallentamiseen.
-setti = {}
+setti = {""}
 while True:
     nimi = input("Syötä nimi: ")
     if nimi == "":
         break
+
     if nimi in setti:
         print("Aiemmin syötetty nimi")
-        setti[nimi] = setti[nimi]+1
-        print(setti[nimi])
     else:
-        setti[nimi] = 1
+        setti.add(nimi)
         print("Uusi nimi")
 
-sortedsetti = dict(sorted(setti.items()))
 
-for nimi in sortedsetti :
-    print(nimi, sortedsetti[nimi])
+for i in setti:
+    print(i)
