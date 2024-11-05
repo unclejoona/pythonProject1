b864e79c4e513a4619cfef7927c1f513f8e267be
Joona Rajaniemi <64476632+unclejoona@users.noreply.github.com>
2024-11-05 14:50:50 +0200

    moduuli 9

--- moduuli 12/2.py
+++ moduuli 12/2.py
@@ -1,7 +1,7 @@
 import requests
 
 paikkakunta = input("Anna kaupunki:")
-api = "cabd1462fbbb60f40462abf33c2348c8"
+api = ""
 
 pyynto = f"https://api.openweathermap.org/data/2.5/weather?q={paikkakunta}&appid={api}&units=metric"
 haku = requests.get(pyynto).json()
